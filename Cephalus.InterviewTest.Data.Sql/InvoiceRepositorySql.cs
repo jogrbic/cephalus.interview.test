69fa1daa2216b736b33dc8d1d40661e2ee7ae430
DeadPool <jo.grbic@outlook.com>
2018-05-09 18:59:40 +0100

    Added Sql Dal implementation

---
+++ Cephalus.InterviewTest.Data.Sql/InvoiceRepositorySql.cs
@@ -0,0 +1,50 @@
+﻿using Cephalus.InterviewTest.Api.Data;
+using Cephalus.InterviewTest.Core.Model;
+using Cephalus.InterviewTest.Data.Sql.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cephalus.InterviewTest.Data.Sql
+{
+	public class InvoiceRepositorySql : DbContext, IInvoiceRepository
+	{
+		public InvoiceRepositorySql(DbContextOptions options)
+			: base(options)
+		{
+		}
+
+		private DbSet<InvoiceSql> Invoices { get; set; }
+
+		public IEnumerable<Invoice> GetAll()
+		{
+			return Set<InvoiceSql>().Select(invoice => ToCore(invoice)).ToArray();
+		}
+
+		public Task<IEnumerable<Invoice>> GetAllAsync()
+		{
+			return Invoices.ToArrayAsync().ContinueWith((t) => t.Result.Select(invoice => ToCore(invoice)));
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<InvoiceSql>().ToTable("Invoice");
+		}
+		
+		// TODO: potentially use auto mapping framework
+		private Invoice ToCore(InvoiceSql invoiceSql)
+		{
+			return new Invoice
+			{
+				Id = invoiceSql.InvoiceId,
+				TaxPointDate = invoiceSql.TaxPointDate,
+				AccuountId = invoiceSql.AccountId,
+				Reference = invoiceSql.Reference,
+				TotalGross = invoiceSql.TotalGross,
+				TotalNet = invoiceSql.TotalNet,
+				TotalVat = invoiceSql.TotalVat
+			};
+		}
+	}
+}
---
+++ Cephalus.InterviewTest.Data.Sql/Model/InvoiceSql.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cephalus.InterviewTest.Data.Sql.Model
+{
+	internal class InvoiceSql
+	{
+		[Key]
+		public int InvoiceId { get; internal set; }
+		public int AccountId { get; set; }
+		public DateTime TaxPointDate { get; internal set; }
+		public string Reference { get; set; }
+		public decimal TotalNet { get; set; }
+		public decimal TotalVat { get; set; }
+		public decimal TotalGross { get; set; }
+	}
+}
