2c0285a40077ba9085e7eb38eed1d0aba2000dd2
DeadPool <jo.grbic@outlook.com>
2018-05-09 18:59:55 +0100

    Added service layer implementation to solution

---
+++ Cephalus.InterviewTest.Core/InvoiceService.cs
@@ -0,0 +1,29 @@
+﻿using Cephalus.InterviewTest.Api.Core;
+using Cephalus.InterviewTest.Api.Data;
+using Cephalus.InterviewTest.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cephalus.InterviewTest.Core
+{
+	public class InvoiceService : IInvoiceService
+	{
+		private IInvoiceRepository _invoiceRepository;
+
+		public InvoiceService(IInvoiceRepository invoiceRepository)
+		{
+			_invoiceRepository = invoiceRepository;
+		}
+
+		public IEnumerable<Invoice> GetInvoices()
+		{
+			return _invoiceRepository.GetAll();
+		}
+
+		public async Task<IEnumerable<Invoice>> GetInvoicesAsync()
+		{
+			return await _invoiceRepository.GetAllAsync();
+		}
+	}
+}
