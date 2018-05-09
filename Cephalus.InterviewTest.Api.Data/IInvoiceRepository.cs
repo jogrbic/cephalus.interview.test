39574776bf2d5286fece3982b6921900c2c2f5e0
DeadPool <jo.grbic@outlook.com>
2018-05-09 18:58:45 +0100

    Added Data Api to the solution

---
+++ Cephalus.InterviewTest.Api.Data/IInvoiceRepository.cs
@@ -0,0 +1,12 @@
+﻿using Cephalus.InterviewTest.Core.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cephalus.InterviewTest.Api.Data
+{
+	public interface IInvoiceRepository
+    {
+		IEnumerable<Invoice> GetAll();
+		Task<IEnumerable<Invoice>> GetAllAsync();
+	}
+}
