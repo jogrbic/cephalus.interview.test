0304eaa17ceac5a4c1d775f1320d7aaa4f5de071
DeadPool <jo.grbic@outlook.com>
2018-05-09 18:58:26 +0100

    Added Api Core project

---
+++ Cephalus.InterviewTest.Api.Core/IInvoiceService.cs
@@ -0,0 +1,12 @@
+﻿using Cephalus.InterviewTest.Core.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Cephalus.InterviewTest.Api.Core
+{
+	public interface IInvoiceService
+    {
+		IEnumerable<Invoice> GetInvoices();
+		Task<IEnumerable<Invoice>> GetInvoicesAsync();
+    }
+}
